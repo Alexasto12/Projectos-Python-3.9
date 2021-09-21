ad1f09cdfe9e8870690fe7086bb4532492a7c93e
Alexasto12 <91148883+Alexasto12@users.noreply.github.com>
2021-09-21 19:53:12 +0200

    Add files via upload

--- Adivina mi numero.py
+++ Adivina mi numero.py
@@ -3,7 +3,13 @@ print("Hola bienvenido a ¡Adivina mi numero!")
 print("Solo tienes que escribir un numero del 1 al 10 para ver si lo adivinas. ¿Seras capaz?")
 Numero_Ganador=random.randint(0,10)
 Numero_Secreto= int(input("Porfavor, escribe tu numero del 0 al 10: "))
+if Numero_Secreto > 10:
+    print("No te flipes tiene que ser menor a 10")
+if Numero_Secreto < 0:
+    print ("¡Te has quedado corto! Tiene que ser mayor a 0")
 if Numero_Secreto!=Numero_Ganador:
     print("¡Has fallado! El numero ganador era: {}".format(Numero_Ganador))
 if Numero_Secreto==Numero_Ganador:
     print ("¡Enhorabuena has acertado!")
+
+
