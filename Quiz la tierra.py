488e165cd1f9d7617e3a572fe79ab0d95233fa19
Alexasto12 <91148883+Alexasto12@users.noreply.github.com>
2021-09-22 19:59:04 +0200

    Add files via upload

---
+++ Quiz la tierra.py
@@ -0,0 +1,66 @@
+Titulo= " Bienvenido, ¿Cuanto sabes de la Tierra?"
+print ("\n"+ Titulo + "\n"+ "_"*27)
+Puntuacion=0
+opcion= input("¿Cuantos años tiene el planeta? \n"
+              "A- 4543,9 millones de años\n"
+              "B- 2021 años\n"
+              "C- 32,5 millones de años\n"
+              "D- 1000000 años\n")
+if opcion=="A":
+    Puntuacion= Puntuacion + 10
+    print("¡Acertaste!")
+elif opcion=="B":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion == "C":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion == "D":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+else:
+    print("Esa no era una respuesta válida")
+print("Tu puntuación es: {} puntos.".format (Puntuacion))
+print("Siguiente pregunta: ")
+opcion= input("¿Como se llamaba el supercontinete antes de que se dividiese? \n"
+              "A- Nueva York\n"
+              "B- Titan\n"
+              "C- Pangea\n"
+              "D- Olympus\n")
+if opcion=="A":
+    Puntuacion= Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion=="B":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion == "C":
+    Puntuacion = Puntuacion + 10
+    print("¡Acertaste!")
+elif opcion == "D":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+else:
+    print("Esa no era una respuesta válida")
+print("Tu puntuación es: {} puntos.".format (Puntuacion))
+print("Siguiente pregunta: ")
+opcion= input("¿Como se llama la linia imaginaria que divide los husos horarios? \n"
+              "A- Translantes\n"
+              "B- Linias de Gaus\n"
+              "C- Meridianos\n"
+              "D- Linias imaginarias\n")
+if opcion=="A":
+    Puntuacion= Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion=="B":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+elif opcion == "C":
+    Puntuacion = Puntuacion + 10
+    print("¡Acertaste!")
+elif opcion == "D":
+    Puntuacion = Puntuacion + 0
+    print("¡Has fallado!")
+else:
+    print("Esa no era una respuesta válida")
+print("Tu puntuación es: {} puntos.".format (Puntuacion))
+
